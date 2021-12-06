53f2e856f4cf0ad75d2e052ffe603a2baf75bcaf
CVitrant <vitrantclementpro@gmail.com>
2021-12-06 16:40:14 +0100

    animation working

---
+++ script/test.js
@@ -0,0 +1,6 @@
+new Vivus('vert', {type: 'delayed', duration: 200, forceRender: false});
+new Vivus('rouge', {type: 'sync', duration: 200, forceRender: false});
+new Vivus('bleu', {type: 'oneByOne', duration: 200, forceRender: false});
+new Vivus('violet', {type: 'delayed', duration: 200, delay : 50, forceRender: false});
+new Vivus('noir', {type: 'scenario', duration: 200, forceRender: false});
+new Vivus('orange', {type: 'scenario-sync', duration: 20, forceRender: false});
